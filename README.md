b19825061666d417bc01f8ab434d09d4d923a682
Fil Peyton <fpeyton@cylance.com>
2014-04-10 12:55:07 -0700

    Gave script scope to prevent collisions in global scope. Added ability to define options. Added new options:     - Ability to define element IDs, animation name     - Define function for fallback when animation isn't supported Updated index documentation, github README documentation.

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 #CSSslidy
 An auto-generated, responsive CSS image slider
+
+### Use:
+* Ensure that all the images you wish to use are exactly the same size.
+* Create an element with an `id` of `slidy-container` and an element with an `id` of `slidy` containing your images:
+```html
+<div id="slidy-container">
+  <figure id="slidy">
+    <img src="eyes.jpg" alt>
+    <img src="lou.jpg" alt>
+    <img src="lucielle.jpg" alt>
+    <img src="lucie.jpg" alt>
+  </figure>
+</div>
+```
+* Add `cssslidy.js` to the bottom of the page.
+* Call `cssSlidy()`:
+```html
+<script type="text/javascript"> cssSlidy(); </script>
+```
+
+### Customize:
+To customize your slidy, pass an options object to the `cssSlidy()` call:
+```html
+<script type="text/javascript">
+  cssSlidy({
+  	timeOnSlide: 5,
+  	timeBetweenSlides: .5,
+  	slidyContainerSelector: 'custom-container',
+  	slidySelector: 'custom-slidy',
+  	fallbackFunction: function(){ alert("Oh noes! You can't animate!"); },
+  	cssAnimationName: 'custom-animation'
+  });
+</script>
+```
+
+
+#### Available options:
+
+Option | Type | Default Value | Description
+---|---|---|---
+`timeOnSlide` | number | `3` | Amount of time (in seconds) per slide
+`timeBetweenSlides` | number | `1` | Amount of time (in seconds) per transition
+`slidyContainerSelector` | string | `'#slidy-container'` | Define the *ID* used for the slidy container element
+`slidySelector` | string | `'#slidy'` | Define the *ID* used for the slidy element
+`fallbackFunction` | function | `function(){}` | Function called if CSS Animation is not supported
+`cssAnimationName` | string | `'slidy'` | Name of CSS animation
--- cssslidy.js
+++ cssslidy.js
@@ -1,58 +1,85 @@
-  /* user defined variables */
-var timeOnSlide = 3, 		
-    // the time each image will remain static on the screen, measured in seconds
-timeBetweenSlides = 1, 	
-    // the time taken to transition between images, measured in seconds
+var cssSlidy = function(newOptions){
+  var options = (function(){
+    var mergedOptions = {},
+      defaultOptions = {
+        timeOnSlide: 3,
+        timeBetweenSlides: 1,
+        slidyContainerSelector: '#slidy-container',
+        slidySelector: '#slidy',
+        fallbackFunction: function(){},
+        cssAnimationName: 'slidy',
+      };
 
-// test if the browser supports animation, and if it needs a vendor prefix to do so
-    animationstring = 'animation',
-    animation = false,
-    keyframeprefix = '',
-    domPrefixes = 'Webkit Moz O Khtml'.split(' '), 
-    // array of possible vendor prefixes
-    pfx  = '',
-    slidy = document.getElementById("slidy"); 
-if (slidy.style.animationName !== undefined) { animation = true; } 
-// browser supports keyframe animation w/o prefixes
+      for( var option in defaultOptions ) mergedOptions[option] = defaultOptions[option];
+      for( var option in newOptions ) mergedOptions[option] = newOptions[option];
 
-if( animation === false ) {
-  for( var i = 0; i < domPrefixes.length; i++ ) {
-    if( slidy.style[ domPrefixes[i] + 'AnimationName' ] !== undefined ) {
-      pfx = domPrefixes[ i ];
-      animationstring = pfx + 'Animation';
-      keyframeprefix = '-' + pfx.toLowerCase() + '-';
-      animation = true;
-      break;
+    return mergedOptions;
+  })(),
+  CS = this;
+
+  CS.animationString = 'animation';
+  CS.hasAnimation = false;
+    CS.keyframeprefix = '';
+    CS.domPrefixes = 'Webkit Moz O Khtml'.split(' ');
+      CS.pfx = '';
+    CS.element = document.getElementById(options.slidySelector.replace('#', ''));
+
+  CS.init = (function(){
+      // browser supports keyframe animation w/o prefixes
+        if (CS.element.style.animationName !== undefined) CS.hasAnimation = true;
+
+        // browser supports keyframe animation w/ prefixes
+      if( CS.hasAnimation === false ) {
+        for( var i = 0; i < CS.domPrefixes.length; i++ ) {
+          if( CS.slidy.style[ CS.domPrefixes[i] + 'AnimationName' ] !== undefined ) {
+            CS.pfx = domPrefixes[i];
+            CS.animationString = pfx + 'Animation';
+            CS.keyframeprefix = '-' + pfx.toLowerCase() + '-';
+            CS.hasAnimation = true;
+            break;
+          }
+        }
+      }
+
+    if( CS.hasAnimation === true ) {
+      var images = CS.element.getElementsByTagName("img"),
+          firstImg = images[0], 
+            // get the first image inside the "slidy" element.
+            imgWrap = firstImg.cloneNode(false);  // copy it.
+            CS.element.appendChild(imgWrap); // add the clone to the end of the images
+
+        var imgCount = images.length, // count the number of images in the slide, including the new cloned element
+            totalTime = (options.timeOnSlide + options.timeBetweenSlides) * (imgCount - 1), // calculate the total length of the animation by multiplying the number of _actual_ images by the amount of time for both static display of each image and motion between them
+            slideRatio = (options.timeOnSlide / totalTime)*100, // determine the percentage of time an induvidual image is held static during the animation
+            moveRatio = (options.timeBetweenSlides / totalTime)*100, // determine the percentage of time for an individual movement
+            basePercentage = 100/imgCount, // work out how wide each image should be in the slidy, as a percentage.
+            position = 0, // set the initial position of the slidy element
+            css = document.createElement("style"); // start marking a new style sheet
+
+        // creating css style tag
+          css.type = "text/css";
+          css.id = options.slidySelector.replace('#', '') + "-css";
+          css.innerHTML += options.slidyContainerSelector + " { overflow: hidden; }\n";
+          css.innerHTML += options.slidySelector + " { margin: 0; font-size: 0; position: relative; width: " + (imgCount * 100) + "%;  }\n"; // set the width for the slidy container
+          css.innerHTML += options.slidySelector + " img { width: " + basePercentage + "%; }\n";
+          css.innerHTML += "@"+keyframeprefix+"keyframes "+options.cssAnimationName+" {\n"; 
+          for (i=0;i<(imgCount-1); i++) { // 
+            position += slideRatio; // make the keyframe the position of the image
+            css.innerHTML += position + "% { left: -"+(i * 100)+"%; }\n";
+            position += moveRatio; // make the postion for the _next_ slide
+            css.innerHTML += position + "% { left: -"+((i+1) * 100)+"%; }\n";
+        }
+        css.innerHTML += "}\n";
+          css.innerHTML += options.slidySelector + " { left: 0%; " + keyframeprefix + "transform: translate3d(0,0,0); " + keyframeprefix + "animation: " + totalTime + "s " + options.cssAnimationName + " infinite; }\n"; // call on the completed keyframe animation sequence
+        
+          // placing css style tag
+          if(options.cssLocation !== undefined)
+            options.cssLocation.appendChild(css);
+          else
+          document.body.appendChild(css);
+    } else {
+        // fallback function
+        options.fallbackFunction();
     }
-  }
-}
-
-if( animation === false ) {
-  // animate in JavaScript fallback
-} else {
-  var images = slidy.getElementsByTagName("img"),
-      firstImg = images[0], 
-      // get the first image inside the "slidy" element.
-      imgWrap = firstImg.cloneNode(false);  // copy it.
-  slidy.appendChild(imgWrap); // add the clone to the end of the images
-  var imgCount = images.length, // count the number of images in the slide, including the new cloned element
-      totalTime = (timeOnSlide + timeBetweenSlides) * (imgCount - 1), // calculate the total length of the animation by multiplying the number of _actual_ images by the amount of time for both static display of each image and motion between them
-      slideRatio = (timeOnSlide / totalTime)*100, // determine the percentage of time an induvidual image is held static during the animation
-      moveRatio = (timeBetweenSlides / totalTime)*100, // determine the percentage of time for an individual movement
-      basePercentage = 100/imgCount, // work out how wide each image should be in the slidy, as a percentage.
-      position = 0, // set the initial position of the slidy element
-      css = document.createElement("style"); // start marking a new style sheet
-  css.type = "text/css";
-  css.innerHTML += "#slidy { margin: 0; font-size: 0; position: relative; width: " + (imgCount * 100) + "%;  }\n"; // set the width for the slidy container
-  css.innerHTML += "#slidy img { width: " + basePercentage + "%; }\n";
-  css.innerHTML += "@"+keyframeprefix+"keyframes slidy {\n"; 
-  for (i=0;i<(imgCount-1); i++) { // 
-    position+= slideRatio; // make the keyframe the position of the image
-    css.innerHTML += position+"% { left: -"+(i * 100)+"%; }\n";
-    position += moveRatio; // make the postion for the _next_ slide
-    css.innerHTML += position+"% { left: -"+((i+1) * 100)+"%; }\n";
-}
-  css.innerHTML += "}\n";
-  css.innerHTML += "#slidy { left: 0%; "+keyframeprefix+"transform: translate3d(0,0,0); "+keyframeprefix+"animation: "+totalTime+"s slidy infinite; }\n"; // call on the completed keyframe animation sequence
-document.body.appendChild(css); // add the new stylesheet to the end of the document
+  })();
 }
